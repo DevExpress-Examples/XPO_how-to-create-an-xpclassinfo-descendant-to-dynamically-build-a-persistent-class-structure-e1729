64bb8e2660ccd6036190ab1f0b9a88b81a7c8d74
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Data;
+using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace DynamicDataTable {
+    public partial class Form1 : Form {
+        private readonly string connectionString = "Data Source=(local);Initial Catalog=Northwind;Integrated Security=True";
+        private SqlConnection connection;
+        private MSSqlConnectionProvider connectionProvider;
+        private Session workSession;
+        private Dictionary<string, DynamicDataTableClassInfo> dynamicClasses;
+        public Form1() {
+            InitializeComponent();
+            Init();
+        }
+        protected void Init() {
+            connection = new SqlConnection(connectionString);
+            connectionProvider = new MSSqlConnectionProvider(connection, AutoCreateOption.None);
+            IDataLayer dal = new SimpleDataLayer(connectionProvider);
+            workSession = new Session(dal);
+            string[] tablesList = connectionProvider.GetStorageTablesList(false);
+            dynamicClasses = new Dictionary<string, DynamicDataTableClassInfo>(tablesList.Length);
+            cmbTables.Properties.Items.AddRange(tablesList);
+            cmbTables.SelectedIndexChanged += delegate(object sender, EventArgs args) {
+                FillGrid(cmbTables.Text);
+            };
+        }
+        protected void FillGrid(string tableName) {
+            grdTableBrowser.DataSource = null;
+            gridView1.OptionsBehavior.Editable = false;
+            gridView1.Columns.Clear();
+            SqlDataAdapter adapter = new SqlDataAdapter(string.Format("SELECT * FROM [{0}]", tableName), connection);
+            DataTable dataTable = new DataTable(tableName);
+            adapter.FillSchema(dataTable, SchemaType.Source);
+            DynamicDataTableClassInfo dataTableClassInfo;
+            if (!dynamicClasses.TryGetValue(tableName, out dataTableClassInfo)) {
+                dynamicClasses.Add(tableName, new DynamicDataTableClassInfo(workSession.Dictionary, dataTable));
+            }
+            XPServerCollectionSource dataSource = new XPServerCollectionSource(workSession, dynamicClasses[tableName]);
+            grdTableBrowser.DataSource = dataSource;
+            //grdTableBrowser.ServerMode = true;
+            grdTableBrowser.ForceInitialize();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to create an XPClassInfo descendant to dynamically build a persistent class structure
+
+
+<p>This example is a variation of <a href="https://www.devexpress.com/Support/Center/p/E1198">How to generate persistent classes at runtime based on a dataset </a> with the difference that here we create a custom <a href="http://documentation.devexpress.com/#XPO/clsDevExpressXpoMetadataXPClassInfotopic">XPClassInfo</a> class that provides metadata information for a type.<br /> Then, we will use it to bind the <a href="http://documentation.devexpress.com/#WPF/clsDevExpressWpfGridGridControltopic">GridControl</a> to the <a href="http://documentation.devexpress.com/#XPO/clsDevExpressXpoXPServerCollectionSourcetopic">XPServerCollectionSource</a>, that is necessary to work in the <a href="http://documentation.devexpress.com/#WindowsForms/CustomDocument2990">Server Mode</a>.</p>
+
+<br/>
+
+
